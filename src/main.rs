2906adf2c95d0392ac5f463d9c75ed340ca384b9
abezen <vmk78aus@yahoo.com.au>
2021-10-06 21:23:28 +1100

    cleaning up the code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 
 pub mod root;
-// use num::{Float, FromPrimitive, Signed, abs};
-// use std::env;
+
 
 
 
 fn main() {
-    let op: u128 = 200000;
-    let ap: u128 = 300000;
-    let of: u128 = 1050;
+    let op: u128 = 300000;
+    let ap: u128 = 400000;
+    let of: u128 = 2150;
     
     let sol1 = root::curve_v1(op, ap, of);
     let sol2 = root::curve_v2(op, ap, of);
